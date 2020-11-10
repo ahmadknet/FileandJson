37ddeafee69a6ea22a8e8e2089dce3797500a1f1
Ahmad Kpro <a.khan@k-protechnologies.com>
2020-11-10 19:04:24 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+   myForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    file: new FormControl('', [Validators.required]),
+    fileSource: new FormControl('', [Validators.required])
+  });
+
+  constructor(private http: HttpClient) { }
+
+  get f(){
+    return this.myForm.controls;
+  }
+
+  onFileChange(event) {
+
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.myForm.patchValue({
+        fileSource: file
+      });
+    }
+  }
+
+  submit(){
+
+    var t =  { "trainername":this.myForm.get('name').value }
+
+    const formData = new FormData();
+
+    formData.append('t', JSON.stringify(t));
+
+    formData.append('avatarphoto', this.myForm.get('fileSource').value);
+
+    this.http.post('http://localhost:44359/api/Reg/Registration', formData)
+      .subscribe(res => {
+        console.log(res);
+        alert('Uploaded Successfully.');
+      })
+  }
 }
